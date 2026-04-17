c6f3ebffd49bbd4f582ae9a40a167fcdca176c1c
MarkusVeesaar <Markus.veesaar@voco.ee>
2026-04-17 13:35:03 +0300

    Displayed an article by slug using the article oop model

--- controllers/article.js
+++ controllers/article.js
@@ -10,6 +10,10 @@ class articleController {
     const articles = await articleModel.findAll();
     res.status(201).json({ articles: articles });
   }
+  async getArticleBySlug(req, res){
+    const article = await articleModel.findOne(req.params.slug)
+    res.status(201).json({article: article})
+  }
 }
 
 module.exports = articleController;
